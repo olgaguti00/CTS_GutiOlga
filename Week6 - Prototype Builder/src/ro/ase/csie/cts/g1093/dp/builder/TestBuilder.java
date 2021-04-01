b77d4dab3f32dc03a27e37f44497ccff68c61a0f
olgaguti00 <gutiolga18@stud.ase.ro>
2021-04-01 14:33:18 +0300

    Prototype implementation. Starting builder

---
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
@@ -0,0 +1,18 @@
+package ro.ase.csie.cts.g1093.dp.builder;
+
+public class TestBuilder {
+
+	public static void main(String[] args) {
+
+		// 1. create the object
+		SuperHero superHero = new SuperHero();
+		// 2.initialize it
+		// TO DO: don't forget
+
+		SuperHero superHero2 = new SuperHero("Superman", 
+				100, false, false, 
+				new Weapon(), null, new Flying(), null);
+
+	}
+
+}
---
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/prorotype/CharacterType.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1093.dp.prorotype;
+
+public enum CharacterType {
+	DISNEY, MARVEL
+}
