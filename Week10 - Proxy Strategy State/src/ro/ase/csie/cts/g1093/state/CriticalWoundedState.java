575571ae8cd74e60382ee44c1e0c3ce6ad317464
olgaguti00 <gutiolga18@stud.ase.ro>
2021-05-06 15:40:07 +0300

    Implementing Strategy and State

---
+++ Week10 - Proxy Strategy State/src/ro/ase/csie/cts/g1093/state/CriticalWoundedState.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.g1093.state;
+
+public class CriticalWoundedState implements MoveStateInterface {
+
+	@Override
+	public void moveAtDifferentLocation() {
+		System.out.println("The hero can't move");
+
+	}
+
+}
---
+++ Week10 - Proxy Strategy State/src/ro/ase/csie/cts/g1093/state/MoveStateInterface.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1093.state;
+
+public interface MoveStateInterface {
+
+	public void moveAtDifferentLocation();
+
+}
