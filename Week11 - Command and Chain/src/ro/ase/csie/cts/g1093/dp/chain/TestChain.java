91ac7da8dd8d9e309f05c542dfc4fccfb6db8ae4
olgaguti00 <gutiolga18@stud.ase.ro>
2021-05-13 14:27:11 +0300

    Implementing Chain

---
+++ Week11 - Command and Chain/src/ro/ase/csie/cts/g1093/dp/chain/TestChain.java
@@ -0,0 +1,22 @@
+package ro.ase.csie.cts.g1093.dp.chain;
+
+public class TestChain {
+
+	public static void main(String[] args) {
+		GenericMessageHandler filterModule = new FilterModule("English");
+		GenericMessageHandler privateModule = new PrivateMessageModule();
+		GenericMessageHandler groupModule = new GroupMessageModule();
+
+		// more restrictive nodes at the beginning
+		filterModule.setNext(privateModule);
+		privateModule.setNext(groupModule);
+
+		GenericMessageHandler chatServer = filterModule;
+
+		chatServer.processMessage(new Message("Hello John", "@John", 1));
+		chatServer.processMessage(new Message("I hate you", "@John", 1));
+		chatServer.processMessage(new Message("Let's join the party", "@everyone", 1));
+
+	}
+
+}
