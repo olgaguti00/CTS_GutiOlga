217b82e17522e2a1ad79802f3809173aed24faaf
olgaguti00 <gutiolga18@stud.ase.ro>
2021-06-03 15:41:31 +0300

    Final commit

--- Week13 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/models/Student.java
+++ Week13 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/models/Student.java
@@ -72,4 +72,14 @@ public class Student {
 		return sum / this.grades.size();
 	}
 
+	public int getMinGrade() {
+		int min = this.grades.get(0);
+		for (int grade : this.grades) {
+			if (min > grade) {
+				min = grade;
+			}
+		}
+		return min;
+	}
+
 }
--- Week13 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudent.java
+++ Week13 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudent.java
@@ -10,15 +10,18 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongAgeException;
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongGradeException;
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongNameException;
 import ro.ase.csie.cts.g1093.testing.models.Student;
+import ro.ase.csie.cts.g1093.testing.tests.categories.ImportantTest;
+import ro.ase.csie.cts.g1093.testing.tests.categories.PerformanceTest;
 
 public class TestStudent {
 
-	//test fixture
+	// test fixture
 	static Student student;
 	static ArrayList<Integer> grades;
 	static String initialName = "John";
@@ -90,6 +93,7 @@ public class TestStudent {
 		student.setName(newName);
 	}
 
+	@Category(ImportantTest.class)
 	@Test
 	public void testGetGradesAverageOrderingAsc() throws WrongGradeException {
 		ArrayList<Integer> grades = new ArrayList<>();
@@ -121,6 +125,7 @@ public class TestStudent {
 		assertEquals("Testing an empty array of grades", expectedAverage, computedAverage, 0);
 	}
 
+	@Category({ ImportantTest.class, PerformanceTest.class })
 	@Test
 	public void testGetGradesAverageCardinalityOne() throws WrongGradeException {
 		ArrayList<Integer> grades = new ArrayList<>();
